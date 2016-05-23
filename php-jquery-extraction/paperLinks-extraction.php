9ae758acd67efa4486236f38940f51a28c21695f
Durval Pereira <contato@durvalpereira.com.br>
2016-05-23 19:27:43 -0300

    updated php extraction

--- php-jquery-extraction/paperLinks-extraction.php
+++ php-jquery-extraction/paperLinks-extraction.php
@@ -9,12 +9,35 @@ $arrContextOptions=array(
 	),
 );
 
+
 $html = str_get_html(file_get_contents("https://academic.microsoft.com/#/search?iq=And(Composite(F.FN%253D%253D'education')%252CComposite(F.FN%253D%253D'informatics'))&q=education%20informatics&filters=&from=0&sort=0", false, stream_context_create($arrContextOptions)));
+$html2 = str_get_html(file_get_contents("https://academic.microsoft.com/#/search?iq=And(Composite(F.FN%253D%253D'education')%252CComposite(F.FN%253D%253D'informatics'))&q=education%20informatics&filters=&from=8&sort=0", false, stream_context_create($arrContextOptions)));
+$html3 = str_get_html(file_get_contents("https://academic.microsoft.com/#/search?iq=And(Composite(F.FN%253D%253D'education')%252CComposite(F.FN%253D%253D'informatics'))&q=education%20informatics&filters=&from=16&sort=0", false, stream_context_create($arrContextOptions)));
 
 $response = array();
 $structure = array();
 
 foreach($html->find('div.content-main') as $element) {
+	$papers = $element->find('a.blue-title');
+	foreach($papers as $paperLinkJson) {
+		$title = $paperLinkJson->title;
+		$link = $paperLinkJson->href;
+
+		$structure[] = array('title' => $title, 'link' => 'link');
+	}
+}
+
+foreach($html2->find('div.content-main') as $element) {
+	$papers = $element->find('a.blue-title');
+	foreach($papers as $paperLinkJson) {
+		$title = $paperLinkJson->title;
+		$link = $paperLinkJson->href;
+
+		$structure[] = array('title' => $title, 'link' => 'link');
+	}
+}
+
+foreach($html3->find('div.content-main') as $element) {
 	$papers = $element->find('a.blue-title');
 	foreach($papers as $paperLinkJson) {
 		$title = $paperLinkJson->title;
